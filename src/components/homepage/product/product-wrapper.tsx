2bc857bbe36e76159f7dc9f6a7ab035cacecd861
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-10 07:53:29 +0545

    hello

---
+++ src/components/homepage/product/product-wrapper.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import productslist from './products-list'
+
+type Props = {}
+
+export default function Productwrapper({}: Props) {
+  return (
+    <div>product-wrapper</div>
+  )
+}
--- src/components/homepage/product/products-list.tsx
+++ src/components/homepage/product/products-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import productDAta from '@/data/product.json'
 import { TProduct } from '@/type/product'
+import Singleproductcard from './single-product-card'
 type Props = {}
 
 export default function productslist({ }: Props) {
