1575474b49deb252cfcbbae548db2650633a3718
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-10 07:45:41 +0545

    milena

--- src/components/homepage/categories-wrapper.tsx
+++ src/components/homepage/categories-wrapper.tsx
@@ -3,7 +3,7 @@ import Singleproductcard from './product/single-product-card'
 
 type Props = {}
 
-export default function categories({}: Props) {
+export default function categorieswrapper({}: Props) {
   return (
     <div>categories</div>
     
